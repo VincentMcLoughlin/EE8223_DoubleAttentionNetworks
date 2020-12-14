9601b98fbb39e23372d1312e3fff465dfcc997dd
Ubuntu <mcvi0001@MachineLearningNC24r.4yt3bipq4p1udhvux1tmuxzc4b.bx.internal.cloudapp.net>
2020-12-14 05:39:28 +0000

    Created results for n=18.

---
+++ results/getData.py
@@ -0,0 +1,30 @@
+
+
+# inputFile = open('resnet_n=7_90_39%.txt', 'r')
+# outputFile = open('n=7_results.txt', 'w') 
+
+# inputFile = open('n=7_initialData.txt', 'r')
+# outputFile = open('n=7_initial_results.txt', 'w') 
+
+# inputFile = open('resnet_n=9_88_86%.txt', 'r')
+# outputFile = open('n=9_results.txt', 'w')
+
+inputFile = open('resnet_n=18_87_66%.txt', 'r')
+outputFile = open('n=18_results.txt', 'w')
+
+lines = inputFile.readlines() 
+numItString = "NumIterations:"
+epochString = "Epoch"
+imagesString = "images: "
+percent = "%"
+for line in lines:
+    numItIndex = line.find(numItString)
+    epochIndex = line.find(epochString)
+    imageIndex = line.find(imagesString)
+    percentIndex=  line.find(percent)
+    if numItIndex != -1:
+        iterations = line[numItIndex+len(numItString):epochIndex].strip()
+        accuracy = line[imageIndex + len(imagesString):percentIndex].strip()
+        resultString = iterations + "," + accuracy + "\n"
+        print(resultString)
+        outputFile.write(resultString)
